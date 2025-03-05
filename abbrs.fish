7fa75ff9cfe87558793d0e225a319014d0deea80
Dave Williams <dave@dave.io>
2025-03-05 18:56:23 +0000

    🔥 Remove tailscale abbreviation from fish shell configuration
    Removed the tailscale abbreviation that pointed to the macOS application path. This abbreviation is no longer needed, possibly because Tailscale is now available in the PATH or being accessed through another method.

--- abbrs.fish
+++ abbrs.fish
@@ -123,7 +123,6 @@ abbr -a sbrse "sudo brew services"
 abbr -a sf "subl ~/.config/fish"
 abbr -a sw switcher
 abbr -a t tlm
-abbr -a tailscale "/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 abbr -a task notion-task
 abbr -a task-dev "ruby -Ilib ./bin/notion-task"
 abbr -a tdl "tidal-dl -l"
