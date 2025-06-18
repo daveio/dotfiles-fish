6f26afa1b5fe02200a7ec0554a69bb2381018d38
Dave Williams <dave@dave.io>
2025-06-18 12:16:26 +0100

    🔧 Update fish shell configuration files
    Changed the yt-dlp abbreviation to use mkv format instead of mp4 for better
    quality and compatibility with various video codecs. Added SLACK_TEAM_ID
    environment variable to vars.fish to enable direct integration with Slack
    workspace in shell scripts and applications.

--- vars.fish
+++ vars.fish
@@ -10,6 +10,7 @@ set -gx OP_PLUGIN_ALIASES_SOURCED 1
 # set -gx PYTHON_BUILD_FREE_THREADING 1
 set -gx SERVERLESS_FRAMEWORK_FORCE_UPDATE true
 set -gx SHOW_ITERM2_WARNING false
+set -gx SLACK_TEAM_ID T03RUU56D
 set -gx SRC $HOME/src
 set -gx SRCHOME $SRC/github.com/daveio
 set -gx TAILSCALE_IPV4 (tailscale ip -4)
